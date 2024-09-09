ee760e22fae76e2c5d3a00ad7be4259ec6a72ba1
Sung Yeob Jung <sung87331@gmail.com>
2024-09-09 17:22:31 +0900

    chapter2

---
+++ typescript/chapter2/examples/1_hello.ts
@@ -0,0 +1,14 @@
+const myName = "Junior Coding";
+const MyAge: number = 25;
+
+console.log(`Hello, ${myName}`);
+console.log(`${MyAge} years olds.`);
+
+let x : number | string;
+x = 1;
+console.log("🚀 ~ x:", x); // Number
+x = "abc";
+console.log("🚀 ~ x:", x); // String
+
+const len = x.length;
+let test = 1
